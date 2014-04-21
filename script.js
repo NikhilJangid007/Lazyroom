2c179e857c806c4e94d51062cc78eeb1ca8191e4
NikhilJangid007 <jangidnik@gmail.com>
2014-04-21 19:28:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -160,12 +160,6 @@ if (API.enabled && $("#radiantscript-css").length <= 0) {
                     API.addChatLog(obj.user.username + " meh'd this track", true);
                 }
             }
-
-            function f_votelggr (obj) {
-                if (obj.grab !=1) {
-             API.sendChat(obj.user.username + " grab this song!", true);
-            }
-           }
             API.on(API.VOTE_UPDATE, f_votelggr);
             API.on(API.CHAT, f_chtlggr);
             // End of chat logger
